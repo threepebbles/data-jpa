9d01a050fc89c4ae33c6997d771860b1e9e9f044
threepebbles <threepebbles@naver.com>
2024-07-30 21:48:06 +0900

    feat: @Query 적용(권장하는 방식)

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package study.datajpa.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
@@ -14,4 +15,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // JpaRepository<T, ID> 에서 T에 해당하는 엔티티에서 NamedQuery를 먼저 탐색함
     // Named Query가 존재하면 Named Query를 실행하고 없으면 `메소드 이름으로 쿼리 생성` 방식으로 쿼리를 생성해서 실행
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -92,4 +92,16 @@ class MemberRepositoryTest {
         Member findMember = result.get(0);
         assertThat(findMember).isEqualTo(member1);
     }
+
+    @Test
+    public void testQuery() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 30);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(member1);
+    }
 }
