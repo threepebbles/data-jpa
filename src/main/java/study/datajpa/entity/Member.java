e6611fecdef2272cf4e2e6538c367801ba71f14a
threepebbles <threepebbles@naver.com>
2024-07-30 20:22:12 +0900

    feat: 예제 Member, Team 모델 동작확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,25 +1,52 @@
 package study.datajpa.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
-// 프록시 객체 사용을 위해 protected까지는 접근제어를 열어놓아야 함
+// JPA의 프록시 객체 기능 사용을 위해 protected까지는 접근제어를 열어놓아야 함
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})   // 연관관계 필드는 toString에 추가 안하는게 좋음. 순환참조 발생 가능성 있음.
 public class Member {
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
+
+    // @~ToOne은 디폴트가 Fettype.EAGER이므로 LAZY로 설정해줘야 함
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
