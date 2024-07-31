ab4d08ec86cb2629ee25ad566562da3ea23ef88f
threepebbles <threepebbles@naver.com>
2024-07-31 20:59:02 +0900

    feat: 페이징과 정렬(local, global 설정 방법)

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,9 +2,15 @@ package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -27,8 +33,24 @@ public class MemberController {
         return member.getUsername();
     }
 
+    // ex) localhost:8080/members?page=1&size=5&sort=username,desc&sort=id,desc
+    // default: page=0, size=20
+    @GetMapping("/members")
+    public Page<MemberDto> list(
+            // local 설정. global 설정은 appliation.yml에서 가능
+            @PageableDefault(page = 1, size = 5, sort = "username", direction = Direction.DESC) Pageable pageable) {
+
+        PageRequest request = PageRequest.of(1, 5);
+        Page<Member> page = memberRepository.findAll(pageable);
+
+        Page<MemberDto> toMap = page.map(member -> new MemberDto(member));
+        return toMap;
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
