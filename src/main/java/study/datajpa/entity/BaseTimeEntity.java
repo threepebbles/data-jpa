ed5b9a97311051c655bd09f046df293580b653f4
threepebbles <threepebbles@naver.com>
2024-07-31 20:15:16 +0900

    comment: 주석 수정

--- src/main/java/study/datajpa/entity/BaseTimeEntity.java
+++ src/main/java/study/datajpa/entity/BaseTimeEntity.java
@@ -14,7 +14,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
     // 시간만 필요하고, 등록자/수정자가 필요 없는 경우 BaseTimeEntity를 상속
-    // 시간과 등록자/수정사 모두 필요한 경우 BaseEntity 상속 
+    // 시간과 등록자/수정자 모두 필요한 경우 BaseEntity 상속 
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdDate;
